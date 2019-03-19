9920db50db46785adb19fd8d2411389473859f68
MJiason <ovr01110@gmail.com>
2019-03-20 00:56:08 +0200

    add Furnitere class, apdate Main.java

---
+++ src/com/company/FurnitureType.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public enum FurnitureType {
+    Chair,
+    Table,
+    Bed
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("text");
+        ArrayList<Furniture> coolMebel = new  ArrayList<>();
+        coolMebel.add(new Furniture("Emperor",500, FurnitureType.Chair, 90, 40,"black" ));
+        coolMebel.add(new Furniture("ArturTable" ,1000, FurnitureType.Table, 95, 200,"brown" ));
+        coolMebel.add(new Furniture("Greece",1340, FurnitureType.Bed, 90, 200,"gray" ));
+        coolMebel.add(new Furniture("BaobabBad",500, FurnitureType.Bed, 90, 40,"red" ));
+        coolMebel.sort(
+                Comparator.comparing(Furniture::getPrice).thenComparing(Furniture::getName)
+        );
+        System.out.println(coolMebel.toString());
     }
 }
