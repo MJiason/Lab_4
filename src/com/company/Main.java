3bb740e0cb6eaf3de50c61e376c37cad83017d22
MJiason <ovr01110@gmail.com>
2019-03-20 09:28:21 +0200

    add execute time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,30 @@
 package com.company;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 
-
-
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<Furniture> coolMebel = new  ArrayList<>();
-        coolMebel.add(new Furniture("Emperor",500, FurnitureType.Chair, 90, 40,"black" ));
-        coolMebel.add(new Furniture("ArturTable" ,1000, FurnitureType.Table, 95, 200,"brown" ));
-        coolMebel.add(new Furniture("Greece",1350, FurnitureType.Bed, 90, 200,"gray" ));
-        coolMebel.add(new Furniture("BaobabBad",550, FurnitureType.Bed, 90, 40,"red" ));
-        coolMebel.add(new Furniture("GlassTable",500, FurnitureType.Table, 100, 45,"green" ));
-        coolMebel.add(new Furniture("SeniorsChair",500, FurnitureType.Chair, 94, 60,"gold" ));
 
-        coolMebel.sort(
-                Comparator.comparing(Furniture::getPrice).thenComparing(Furniture::getName, Comparator.reverseOrder())
-        );
-        System.out.println(coolMebel.toString());
+    public static void main(String[] args) {
+        final long startTime = System.currentTimeMillis();
+
+            ArrayList<Furniture> coolMebel = new ArrayList<>();
+            coolMebel.add(new Furniture("Emperor", 500, FurnitureType.Chair, 90, 40, "black"));
+            coolMebel.add(new Furniture("ArturTable", 1000, FurnitureType.Table, 95, 200, "brown"));
+            coolMebel.add(new Furniture("Greece", 1350, FurnitureType.Bed, 90, 200, "gray"));
+            coolMebel.add(new Furniture("BaobabBad", 550, FurnitureType.Bed, 90, 40, "red"));
+            coolMebel.add(new Furniture("GlassTable", 500, FurnitureType.Table, 100, 45, "green"));
+            coolMebel.add(new Furniture("SeniorsChair", 500, FurnitureType.Chair, 94, 60, "gold"));
+
+            coolMebel.sort(
+                    Comparator.comparing(Furniture::getPrice).thenComparing(Furniture::getName, Comparator.reverseOrder())
+            );
+            System.out.println(coolMebel.toString());
+        
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime - startTime));
+
     }
+
 }
